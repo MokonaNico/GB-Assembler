a050b03949aa43df25de96b0094aa2966a2ec3c1
MokonaNico <mine-nico@hotmail.com>
2023-08-20 02:01:03 +0200

    add some template code for the reading and writing of files

--- src/main.cpp
+++ src/main.cpp
@@ -10,27 +10,36 @@ int main(int argc, char**argv){
                    "The input file", CommandLineParser::FlagType::STRING);
     parser.addFlag("--output", "-o", std::string("output.gb"),
                    "The output file", CommandLineParser::FlagType::STRING);
-    parser.addFlag("--aaa", "-a", false,
-                   "test", CommandLineParser::FlagType::BOOL);
-    parser.addFlag("--bbb", "-b", 27,
-                   "test2", CommandLineParser::FlagType::INT);
     if (parser.parse(argc, argv)) return 0;
 
-    std::cout << std::any_cast<std::string>(parser.getFlagValue("--input")) << std::endl;
-    std::cout << std::any_cast<std::string>(parser.getFlagValue("--output")) << std::endl;
-    std::cout << std::any_cast<bool>(parser.getFlagValue("--aaa")) << std::endl;
-    std::cout << std::any_cast<int>(parser.getFlagValue("--bbb")) << std::endl;
+    auto input_filename = std::any_cast<std::string>(parser.getFlagValue("--input"));
+    auto output_filename = std::any_cast<std::string>(parser.getFlagValue("--output"));
 
-    /*
     std::ifstream inputFile; 
-    inputFile.open("input.asm");
+    inputFile.open(input_filename);
     if (!inputFile.is_open()) {
-        std::cerr << "Erreur lors de l'ouverture du fichier d'entrée." << std::endl;
+        std::cerr << "Error while trying to open the input file." << std::endl;
         return 1;
     }
 
-    inputFile.close();
-    */
+    std::string myText;
+    while (getline(inputFile, myText)) {
+        // Output the text from the file
+        std::cout << myText;
+    }
 
+
+    std::ofstream outputFile;
+    outputFile.open(output_filename, std::ios::out | std::ios::binary);
+    if (!outputFile.is_open()) {
+        std::cerr << "Error while trying to create the output file." << std::endl;
+        return 1;
+    }
+    std::vector<uint8_t> hexVector = {0xFF, 0x00, 0xAA, 0x55};
+    for(uint8_t value : hexVector)
+        outputFile.put((char) value);
+
+    outputFile.close();
+    inputFile.close();
     return 0;
 }
